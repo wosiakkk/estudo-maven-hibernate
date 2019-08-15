0178d172e1be53c5f79fc6fc4220cc4c9a27304b
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-08-15 17:51:33 -0300

    Atualização 3 NOVO
    - Projeto transformado para JSF/WEB para estudos de JSF PRIMEFACES com Hibernate
    - Configurações

---
+++ src/main/java/managedbean/UsuarioPessoaManagedBean.java
@@ -0,0 +1,28 @@
+package managedbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
+@ManagedBean(name = "usuarioPessoaManagedBean")
+@ViewScoped
+public class UsuarioPessoaManagedBean {
+
+	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
+	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+	
+	
+	
+	
+	public UsuarioPessoa getUsuarioPessoa() {
+		return usuarioPessoa;
+	}
+	public void setUsuarioPessoa(UsuarioPessoa usuarioPessoa) {
+		this.usuarioPessoa = usuarioPessoa;
+	}
+	
+	
+	
+}
