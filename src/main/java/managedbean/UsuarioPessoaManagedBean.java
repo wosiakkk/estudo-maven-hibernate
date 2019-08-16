be4997a0a7b84d3cacd476c2c1038b450e6dfcca
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-08-15 22:25:40 -0300

    Atualização 4
    -Formulario para cadstro e datable implementados

--- src/main/java/managedbean/UsuarioPessoaManagedBean.java
+++ src/main/java/managedbean/UsuarioPessoaManagedBean.java
@@ -1,7 +1,12 @@
 package managedbean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import dao.DaoGeneric;
 import model.UsuarioPessoa;
@@ -12,7 +17,7 @@ public class UsuarioPessoaManagedBean {
 
 	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
 	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
-	
+	private List<UsuarioPessoa> list =  new ArrayList<UsuarioPessoa>();
 	
 	
 	
@@ -23,6 +28,25 @@ public class UsuarioPessoaManagedBean {
 		this.usuarioPessoa = usuarioPessoa;
 	}
 	
+	public String salvar() {
+		daoGeneric.salvar(usuarioPessoa);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação: ","Salvo com Sucesso!"));
+		return "";
+	}
 	
+	public String novo() { //método para limpar o formulário e criar um novo objeto
+		usuarioPessoa = new UsuarioPessoa();
+		return "";
+	}
+	
+	public List<UsuarioPessoa> getList() {
+		list = daoGeneric.listar(UsuarioPessoa.class);
+		return list;
+	}
+	public String remover() {
+		daoGeneric.deletarPorId(usuarioPessoa);
+		novo();
+		return "";
+	}
 	
 }
