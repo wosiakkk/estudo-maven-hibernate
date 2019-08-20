75c7e24b6e8d2079de1dec4dab9abde3a7589e72
Bruno Wosiak <31929337+wosiakkk@users.noreply.github.com>
2019-08-20 00:08:28 -0300

    Salvar telefones Lógica para salvar números de telefones implementada

--- src/main/java/managedbean/TelefoneManagedBean.java
+++ src/main/java/managedbean/TelefoneManagedBean.java
@@ -1,11 +1,14 @@
 package managedbean;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import dao.DaoTelefones;
 import dao.DaoUsuario;
+import model.TelefoneUser;
 import model.UsuarioPessoa;
 
 @ManagedBean(name = "telefoneManagedBean")
@@ -14,6 +17,8 @@ public class TelefoneManagedBean {
 	
 	private UsuarioPessoa user = new UsuarioPessoa();
 	private DaoUsuario<UsuarioPessoa> dao = new DaoUsuario<UsuarioPessoa>();
+	private DaoTelefones<TelefoneUser> daoTelefone = new DaoTelefones<TelefoneUser>();
+	private TelefoneUser telefone = new TelefoneUser();
 	
 	@PostConstruct
 	public void init() {
@@ -22,10 +27,34 @@ public class TelefoneManagedBean {
 		user = dao.pesquisar(Long.parseLong(codUser), UsuarioPessoa.class);
 	}
 	
+	public String salvar() {
+		telefone.setUsuarioPessoa(user);;
+		daoTelefone.salvar(telefone);
+		telefone = new TelefoneUser();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação: ","Salvo com Sucesso!"));
+		
+		return "";
+	}
+	
 	public void setUser(UsuarioPessoa user) {
 		this.user = user;
 	}
 	public UsuarioPessoa getUser() {
 		return user;
 	}
+	
+	public void setDaoTelefone(DaoTelefones<TelefoneUser> daoTelefone) {
+		this.daoTelefone = daoTelefone;
+	}
+	public DaoTelefones<TelefoneUser> getDaoTelefone() {
+		return daoTelefone;
+	}
+	
+	public void setTelefone(TelefoneUser telefone) {
+		this.telefone = telefone;
+	}
+	public TelefoneUser getTelefone() {
+		return telefone;
+	}
+	
 }
